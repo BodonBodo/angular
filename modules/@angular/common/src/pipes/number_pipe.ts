BodonBodo/angular

commit 7498050421196ec4f1c401e54be3790fef4db361

refactor: misc (#9308)

modules/@angular/common/src/pipes/number_pipe.ts
@@ -1,13 +1,11 @@
 import {Pipe, PipeTransform} from '@angular/core';
-
 import {BaseException} from '../facade/exceptions';
 import {NumberFormatStyle, NumberFormatter} from '../facade/intl';
 import {NumberWrapper, RegExpWrapper, Type, isBlank, isNumber, isPresent} from '../facade/lang';
-
 import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
 
 var defaultLocale: string = 'en-US';
-var _re = RegExpWrapper.create('^(\\d+)?\\.((\\d+)(\\-(\\d+))?)?$');
+const _NUMBER_REGEXP = /^(\d+)?\.((\d+)(\-(\d+))?)?$'/g;
 
 /**
  * Internal function to format numbers used by Decimal, Percent and Date pipes.
@@ -21,7 +19,7 @@ function formatNumber(
   }
   var minInt = 1, minFraction = 0, maxFraction = 3;
   if (isPresent(digits)) {
-    var parts = RegExpWrapper.firstMatch(_re, digits);
+    var parts = RegExpWrapper.firstMatch(_NUMBER_REGEXP, digits);
     if (isBlank(parts)) {
       throw new BaseException(`${digits} is not a valid digit info for number pipes`);
     }

modules/@angular/common/src/pipes/async_pipe.ts
@@ -1,8 +1,6 @@
-import {ChangeDetectorRef, Injectable, OnDestroy, Pipe, WrappedValue} from '@angular/core';
-
+import {ChangeDetectorRef, OnDestroy, Pipe, WrappedValue} from '@angular/core';
 import {EventEmitter, Observable, ObservableWrapper} from '../facade/async';
 import {isBlank, isPresent, isPromise} from '../facade/lang';
-
 import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
 
 interface SubscriptionStrategy {
@@ -63,7 +61,6 @@ var __unused: Promise<any>;  // avoid unused import when Promise union types are
  * @stable
  */
 @Pipe({name: 'async', pure: false})
-@Injectable()
 export class AsyncPipe implements OnDestroy {
   /** @internal */
   _latestValue: Object = null;

modules/@angular/common/src/pipes/date_pipe.ts
@@ -1,11 +1,8 @@
-import {Injectable, Pipe, PipeTransform} from '@angular/core';
-
+import {Pipe, PipeTransform} from '@angular/core';
 import {isDate, isNumber, isString, DateWrapper, isBlank,} from '../facade/lang';
 import {DateFormatter} from '../facade/intl';
 import {StringMapWrapper} from '../facade/collection';
-
 import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
-
 
 // TODO: move to a global configurable location along with other i18n components.
 var defaultLocale: string = 'en-US';
@@ -80,7 +77,6 @@ var defaultLocale: string = 'en-US';
  * @experimental
  */
 @Pipe({name: 'date', pure: true})
-@Injectable()
 export class DatePipe implements PipeTransform {
   /** @internal */
   static _ALIASES: {[key: string]: String} = {

modules/@angular/common/src/pipes/i18n_plural_pipe.ts
@@ -1,10 +1,8 @@
 import {Pipe, PipeTransform} from '@angular/core';
-
-import {RegExpWrapper, StringWrapper, isPresent, isStringMap} from '../facade/lang';
-
+import {StringWrapper, isPresent, isStringMap} from '../facade/lang';
 import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
 
-var interpolationExp: RegExp = RegExpWrapper.create('#');
+const _INTERPOLATION_REGEXP: RegExp = /#/g;
 
 /**
  *
@@ -51,6 +49,6 @@ export class I18nPluralPipe implements PipeTransform {
     key = value === 0 || value === 1 ? `=${value}` : 'other';
     valueStr = isPresent(value) ? value.toString() : '';
 
-    return StringWrapper.replaceAll(pluralMap[key], interpolationExp, valueStr);
+    return StringWrapper.replaceAll(pluralMap[key], _INTERPOLATION_REGEXP, valueStr);
   }
 }

modules/@angular/common/src/pipes/i18n_select_pipe.ts
@@ -1,8 +1,6 @@
 import {Pipe, PipeTransform} from '@angular/core';
-
 import {StringMapWrapper} from '../facade/collection';
 import {isStringMap} from '../facade/lang';
-
 import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
 
 

modules/@angular/common/src/pipes/lowercase_pipe.ts
@@ -1,7 +1,5 @@
 import {Pipe, PipeTransform} from '@angular/core';
-
 import {isBlank, isString} from '../facade/lang';
-
 import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
 
 

modules/@angular/common/src/pipes/replace_pipe.ts
@@ -1,7 +1,5 @@
-import {Injectable, Pipe, PipeTransform} from '@angular/core';
-
+import {Pipe, PipeTransform} from '@angular/core';
 import {RegExpWrapper, StringWrapper, isBlank, isFunction, isNumber, isString} from '../facade/lang';
-
 import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
 
 /**
@@ -35,7 +33,6 @@ import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
  */
 
 @Pipe({name: 'replace'})
-@Injectable()
 export class ReplacePipe implements PipeTransform {
   transform(value: any, pattern: string|RegExp, replacement: Function|string): any {
     if (isBlank(value)) {

modules/@angular/common/src/pipes/slice_pipe.ts
@@ -1,8 +1,6 @@
 import {Pipe, PipeTransform} from '@angular/core';
-
 import {ListWrapper} from '../facade/collection';
 import {StringWrapper, isArray, isBlank, isString} from '../facade/lang';
-
 import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
 
 /**

commit 13939b5cccab73cfa664a233d4cb0e171e2ce71c

fix(NumberPipe): fix broken RegExp
introduced in 7498050421196ec4f1c (#9308)

modules/@angular/common/src/pipes/number_pipe.ts
@@ -5,7 +5,7 @@ import {NumberWrapper, RegExpWrapper, Type, isBlank, isNumber, isPresent} from '
 import {InvalidPipeArgumentException} from './invalid_pipe_argument_exception';
 
 var defaultLocale: string = 'en-US';
-const _NUMBER_REGEXP = /^(\d+)?\.((\d+)(\-(\d+))?)?$'/g;
+const _NUMBER_FORMAT_REGEXP = /^(\d+)?\.((\d+)(\-(\d+))?)?$/g;
 
 /**
  * Internal function to format numbers used by Decimal, Percent and Date pipes.
@@ -19,7 +19,7 @@ function formatNumber(
   }
   var minInt = 1, minFraction = 0, maxFraction = 3;
   if (isPresent(digits)) {
-    var parts = RegExpWrapper.firstMatch(_NUMBER_REGEXP, digits);
+    var parts = RegExpWrapper.firstMatch(_NUMBER_FORMAT_REGEXP, digits);
     if (isBlank(parts)) {
       throw new BaseException(`${digits} is not a valid digit info for number pipes`);
     }

modules/@angular/common/test/pipes/number_pipe_spec.ts
@@ -19,7 +19,6 @@ export function main() {
             expect(pipe.transform(123, '.2')).toEqual('123.00');
             expect(pipe.transform(1, '3.')).toEqual('001');
             expect(pipe.transform(1.1, '3.4-5')).toEqual('001.1000');
-
             expect(pipe.transform(1.123456, '3.4-5')).toEqual('001.12346');
             expect(pipe.transform(1.1234)).toEqual('1.123');
           });
@@ -54,6 +53,7 @@ export function main() {
           it('should return correct value for numbers', () => {
             expect(pipe.transform(123)).toEqual('USD123');
             expect(pipe.transform(12, 'EUR', false, '.2')).toEqual('EUR12.00');
+            expect(pipe.transform(5.123, 'USD', false, '.0-2')).toEqual('USD5.12');
           });
 
           it('should not support other objects',